103d3dcdfec7cfd1273d7f1d0b611b9fe53aee35
Marcus Russi <marcusrussi@gmail.com>
2019-04-16 15:53:30 -0400

    parallel.sbatch, tbabm.sh for fast cluster runs of many RunSheets

--- src/tbabm.sh
+++ src/tbabm.sh
@@ -10,5 +10,12 @@ echo param $1 seq:$PARALLEL_SEQ host:$(hostname) date:$(date)
 
 container_exec="singularity exec ../../../containers_latest.sif"
 
-$container_exec ./TBABM -t $1 -m $2 -n $3 -p params_$4 -o results_$4/
+n_trajectories=4
+n_threads=$1
+n_people=31500
 
+$container_exec ./TBABM -t $n_trajectories \
+                        -m $n_threads \
+                        -n $n_people \
+                        -p rangefile_medium_$2 \
+                        -o rangefile_medium_results_$2/
